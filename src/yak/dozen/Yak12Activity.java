3162e08d327e278ce4d22adf4d31a31723a55fe8
Strick Yak <strickyak@gmail.com>
2014-07-08 00:28:12 -0700

    slotting Experimental MessengerService

--- src/yak/dozen/Yak12Activity.java
+++ src/yak/dozen/Yak12Activity.java
@@ -40,10 +40,18 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Canvas.VertexMode;
 import android.graphics.Color;
 import android.util.Log;
@@ -507,5 +515,140 @@ public class Yak12Activity extends Activity {
 
 	LayoutParams FILL = new LayoutParams(LayoutParams.FILL_PARENT,
 			LayoutParams.FILL_PARENT);
+	
 
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// Notification Experiments
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//     http://developer.android.com/reference/android/app/Service.html#onCreate()
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+//	void foo() {
+//		Context context = Yak12Activity.this;
+//		
+//		context = context.getApplicationContext();
+//		
+//		NotificationManager notificationManager = (NotificationManager) context
+//		    .getSystemService(NOTIFICATION_SERVICE);
+//		
+//		
+//		Notification updateComplete = new Notification();
+//		updateComplete.icon = android.R.drawable.stat_notify_sync;
+//		updateComplete.tickerText = context
+//		    .getText(R.string.notification_title);
+//		updateComplete.when = System.currentTimeMillis();
+//	}
+	
+	/** Messenger for communicating with service. */
+	Messenger mService = null;
+	/** Flag indicating whether we have called bind on the service. */
+	boolean mIsBound;
+	/** Some text view we are using to show state information. */
+	TextView mCallbackText;
+
+	/**
+	 * Handler of incoming messages from service.
+	 */
+	class IncomingHandler extends Handler {
+	    @Override
+	    public void handleMessage(Message msg) {
+	        switch (msg.what) {
+	            case MessengerService.MSG_SET_VALUE:
+	                mCallbackText.setText("Received from service: " + msg.arg1);
+	                break;
+	            default:
+	                super.handleMessage(msg);
+	        }
+	    }
+	}
+
+	/**
+	 * Target we publish for clients to send messages to IncomingHandler.
+	 */
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+	/**
+	 * Class for interacting with the main interface of the service.
+	 */
+	private ServiceConnection mConnection = new ServiceConnection() {
+	    public void onServiceConnected(ComponentName className,
+	            IBinder service) {
+	        // This is called when the connection with the service has been
+	        // established, giving us the service object we can use to
+	        // interact with the service.  We are communicating with our
+	        // service through an IDL interface, so get a client-side
+	        // representation of that from the raw service object.
+	        mService = new Messenger(service);
+	        mCallbackText.setText("Attached.");
+
+	        // We want to monitor the service for as long as we are
+	        // connected to it.
+	        try {
+	            Message msg = Message.obtain(null,
+	                    MessengerService.MSG_REGISTER_CLIENT);
+	            msg.replyTo = mMessenger;
+	            mService.send(msg);
+
+	            // Give it some value as an example.
+	            msg = Message.obtain(null,
+	                    MessengerService.MSG_SET_VALUE, this.hashCode(), 0);
+	            mService.send(msg);
+	        } catch (RemoteException e) {
+	            // In this case the service has crashed before we could even
+	            // do anything with it; we can count on soon being
+	            // disconnected (and then reconnected if it can be restarted)
+	            // so there is no need to do anything here.
+	        }
+
+	        // As part of the sample, tell the user what happened.
+	        Toast.makeText(binding, R.string.remote_service_connected,
+	                Toast.LENGTH_SHORT).show();
+	    }
+
+	    public void onServiceDisconnected(ComponentName className) {
+	        // This is called when the connection with the service has been
+	        // unexpectedly disconnected -- that is, its process crashed.
+	        mService = null;
+	        mCallbackText.setText("Disconnected.");
+
+	        // As part of the sample, tell the user what happened.
+	        Toast.makeText(binding, R.string.remote_service_disconnected,
+	                Toast.LENGTH_SHORT).show();
+	    }
+	};
+
+	void doBindService() {
+	    // Establish a connection with the service.  We use an explicit
+	    // class name because there is no reason to be able to let other
+	    // applications replace our component.
+	    bindService(new Intent(binding, 
+	            MessengerService.class), mConnection, Context.BIND_AUTO_CREATE);
+	    mIsBound = true;
+	    mCallbackText.setText("Binding.");
+	}
+
+	void doUnbindService() {
+	    if (mIsBound) {
+	        // If we have received the service, and hence registered with
+	        // it, then now is the time to unregister.
+	        if (mService != null) {
+	            try {
+	                Message msg = Message.obtain(null,
+	                        MessengerService.MSG_UNREGISTER_CLIENT);
+	                msg.replyTo = mMessenger;
+	                mService.send(msg);
+	            } catch (RemoteException e) {
+	                // There is nothing special we need to do if the service
+	                // has crashed.
+	            }
+	        }
+
+	        // Detach our existing connection.
+	        unbindService(mConnection);
+	        mIsBound = false;
+	        mCallbackText.setText("Unbinding.");
+	    }
+	}
+
+	Context binding = Yak12Activity.this;  // In example, was Binding.this.
 }
